024e73bea79876a97ebf2833cd0a1d0aec2a4378
Michael Austin <maustinfl@gmail.com>
2025-06-03 05:48:52 -0700

    Revert "Better updating of mouse collisions, properly remove pairing elements when BuildableObjects are destroyed"
    This reverts commit 81a60a3565758228f23b4555d7967b77e8f67b23.

---
+++ Patches/SerializedMonoBehaviourInitializePostfix.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using LinkedMovement;
+using System;
+using System.Reflection;
+
+#nullable disable
+[HarmonyPatch]
+class SerializedMonoBehaviourInitializePostfix {
+    [HarmonyTargetMethod]
+    static MethodBase TargetMethod() {
+        MethodBase methodBase = (MethodBase)AccessTools.Method(typeof(SerializedMonoBehaviour), "Initialize");
+        if (methodBase != null) {
+            LinkedMovement.LinkedMovement.Log("SerializedMonoBehaviour.Initialize method found");
+        } else {
+            LinkedMovement.LinkedMovement.Log("SerializedMonoBehaviour.Initialize method NOT FOUND");
+        }
+        return methodBase;
+    }
+
+    [HarmonyPostfix]
+    static void Initialize(SerializedMonoBehaviour __instance) {
+        var bo = __instance as BuildableObject;
+        if (bo != null) {
+            var baseTransform = bo.transform;
+            bool foundPlatform = false;
+
+            var baseChildrenCount = baseTransform.childCount;
+            for (var i = 0; i < baseChildrenCount; i++) {
+                var child = baseTransform.GetChild(i);
+                var childName = child.gameObject.name;
+                if (childName.Contains("[Platform]")) {
+                    foundPlatform = true;
+                    break;
+                }
+            }
+            if (foundPlatform) {
+                LinkedMovement.LinkedMovement.Log("Found Platform, Initialize");
+                LinkedMovement.LinkedMovement.GetController().addPlatformObject(bo);
+            }
+        }
+    }
+}
